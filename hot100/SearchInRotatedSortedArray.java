53bb8c75b9170843b5ec49efd8dd2ab1fa42ab0b
zhangweilin <dzxxgczwl@bupt.edu.cn>
2023-02-22 23:55:20 +0800

    33. Search in Rotated Sorted Array(middle)

---
+++ hot100/SearchInRotatedSortedArray.java
@@ -0,0 +1,40 @@
+package hot100;
+
+public class SearchInRotatedSortedArray {
+    public int searchInRotatedSortedArray(int[] nums,int target){
+        int len = nums.length;
+        if(len==0){
+            return -1;
+        }
+        if(len==1){
+            return nums[0]==target?0:-1;
+        }
+        int left = 0;
+        int right = len-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[0]<=nums[mid]){
+                // 左边有序区间
+                if(nums[0]<=target&&target<nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }else{
+                // 右边有序区间
+                if(nums[mid]<target&&target<=nums[len-1]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+
+
+    }
+    
+}
