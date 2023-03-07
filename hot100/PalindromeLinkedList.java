62c83db0aff158ba63955bd427e43444e4006dd0
zhangweilin <416880711@qq.com>
2023-03-08 00:55:37 +0800

    234. Palindrome Linked List（easy)

---
+++ hot100/PalindromeLinkedList.java
@@ -0,0 +1,25 @@
+package hot100;
+
+public class PalindromeLinkedList {
+
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        int[] arr = list.stream().mapToInt(Integer::valueOf).toArray();
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+
+    }
+}
